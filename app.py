689b502302722647afe7e609ee318ee7548591a3
rutili <85592867+rutili@users.noreply.github.com>
2021-12-07 17:37:23 -0600

    working html and flask

--- app.py
+++ app.py
@@ -1,19 +1,37 @@
 import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
-import joblib
+import pandas as pd
+
+
 
 app = Flask(__name__)
 
-def ValuePredictor(to_predict_list):
+def ValuePredictor_LR(final_input):
     col_list = ["pclass",	"sex",	"age"	,"sibsp" ,"	parch"	,"fare"]
-    #model_LogisticRegression = joblib.load('logistic_reg.sav')
-    
+    #model_LogisticRegression = pickle.load('titanicData.pkl')
+    #titanicData_Scaler.pkl
+    print(final_input)
+    print("Inside LR")
+    #Loading the saved decision tree model pickle
+    Logistic_model_pkl  = open('titanicData_LR.pkl', 'rb')
+    LogisticReg_model = pickle.load(Logistic_model_pkl )
+    pred = LogisticReg_model.predict(final_input)
+    print ("Logistic Regression model :: ", pred)
+    return pred
 
-# Loading the saved decision tree model pickle
-#decision_tree_model_pkl = open(decision_tree_pkl_filename, 'rb')
-#decision_tree_model = pickle.load(decision_tree_model_pkl)
-#print "Loaded Decision tree model :: ", decision_tree_model
+def ValuePredictor_SVM(final_input):
+    #col_list = ["pclass",	"sex",	"age"	,"sibsp" ,"	parch"	,"fare"]
+    #model_LogisticRegression = pickle.load('titanicData.pkl')
+    #titanicData_Scaler.pkl
+    print("Inside LR")
+    print(final_input)
+    #Loading the saved decision tree model pickle
+    SVM_model_pkl  = open('titanicData_SVM.pkl', 'rb')
+    SVM_model = pickle.load(SVM_model_pkl )
+    pred = SVM_model.predict(final_input)
+    print ("SVM model :: ", pred)
+    return pred
 
 @app.route('/')
 def index():
@@ -51,36 +69,75 @@ def vis6():
 def vis7():
     return render_template('vis7.html')
 
+
+
 @app.route('/predict',methods=['GET','POST'])
 def predict():
+
     if request.method == 'POST':
      #parse form request in json format
      #parse_request = request.json
      #print(json.dumps(parse_request, indent=4, sort_keys=True))
      #ValuePredictor(parse_request)
      #  
-     
-     age = request.form["age"]
-     pclass = request.form["pclass"]
-     gender = request.form["gender"]
-     parch = request.form["parch"]
-     sibsp = request.form["sibsp"]
-     fare = request.form["fare"]
+     pickModel =  int(request.form["Pick_model"])
+     age = float(request.form["age"])
+     pclass = int(request.form["pclass"])
+     gender = int(request.form["gender"])
+     parch = int(request.form["parch"])
+     sibsp = int(request.form["sibsp"])
+     fare = float(request.form["fare"])
+     print(pickModel)
 
     print(age)
     print(pclass)
     print(gender)
+
     print(parch)
     print(sibsp)
+
     print(fare)
-    return jsonify(age)
-
+    new_titanic_data =[age,pclass,gender,parch,sibsp,fare]
+    #ValuePredictor_LR(new_titanic_data)
+    #return jsonify(age)
+    #return jsonify(new_titanic_data)
      #array to pass 
-    #final_input = [np.array(parse_request)]
+    final_input = [np.array( new_titanic_data)]
 
-   # predict = ValuePredictor
-    #return render_template('index.html')
+    if pickModel == 0:
+        pred = ValuePredictor_LR(final_input)
+    else:
+        pred = ValuePredictor_SVM(final_input)
+    #return pred
+    #print(pred)
+
+    Final_output = round(pred[0], 2)
+    print(Final_output)
+    if Final_output == 0:
+         prediction_LR ='Not Survived'
+    else:
+        prediction_LR= 'Survived'
+
+    titanic_data= {}
+    titanic_data['age'] = age
+    titanic_data['Passenger_Class'] =pclass
+    titanic_data['Sex'] =gender
+    titanic_data['parch']= parch
+    titanic_data['sibsp'] = sibsp
+    titanic_data['Fare'] =fare
+    titanic_data['Prediction'] =prediction_LR
    
+
+    titanic_outputdata = pd.DataFrame({'AGE':[age], 'PASSENGER_CLASS':[pclass],'SEX':[gender],'PARCH':[parch],'SIBBLING':[sibsp],'FARE':[fare],'PREDICTION':[prediction_LR]})
+    print(titanic_outputdata)
+
+    #return pred
+    #predict = ValuePredictor
+    #return render_template('results.html',pred=pred)
+    #return render_template('results.html',prediction_LR='Survival prediction {}'.format(Final_output))
+    #return render_template('results.html',prediction_LR=titanic_outputdata)
+    return render_template('results.html',prediction_model=titanic_data)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
